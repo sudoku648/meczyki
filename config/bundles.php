d42a134ea6d989f2fd4657b395b82929a78cae02
sudoku648 <sudoku648@gmail.com>
2023-12-22 12:09:37 +0100

    Remove API and update packages

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                   => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class                 => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class                       => ['dev' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class                 => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class        => ['dev' => true, 'test' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class                          => ['all' => true],
     WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle::class => ['all' => true],
